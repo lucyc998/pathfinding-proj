356b6d61d8233a08f3a8e5250230554cc6304cca
sangjoon245 <53383599+sangjoon245@users.noreply.github.com>
2021-01-01 15:42:50 -0500

    Changes to Buttons

--- project classes/src/MainFrame.java
+++ project classes/src/MainFrame.java
@@ -1,23 +1,40 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class MainFrame extends JFrame{
 
     MainFrame() {
         this.setTitle("Pathfinder Visualizer");
+        this.setSize(500,500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.getContentPane().setLayout(new BorderLayout(50,100));
         this.add(new Grid(), BorderLayout.CENTER);
-        this.add(new SelectionPanel(), BorderLayout.PAGE_END);
-        this.pack();
-
-        this.setVisible(true);
-        this.setLocationRelativeTo(null); //centers middle of computer
         
-        if(5 == 0){
-            
-        }
+        SelectionPanel p = new SelectionPanel();
+        
+        String options[] = {"Starting Point", "Ending Point", "Wall"};
+        
+        JComboBox comboBox = new JComboBox(options);
+        
+        JButton startButton = new JButton("Start");
+        JButton resetButton = new JButton("Reset");
+        
+        
+        p.add(startButton);
+        p.add(resetButton);
+        p.add(comboBox);
+        
+        comboBox.setSize(500,500);
+        this.add(p, BorderLayout.PAGE_END);
+        
+        pack();
+
+        this.setLayout(null);
+        this.setLocationRelativeTo(null); //centers middle of computer
+        this.setVisible(true);
 
     }
 }
+
